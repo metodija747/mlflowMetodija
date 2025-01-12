ae63b6e7ad72e0267ba1978d87506af7cfca2d9f
metodija747 <metodijabucevski@yahoo.com>
2025-01-12 09:26:20 +0100

    ghvgnvb

--- s3.tf
+++ s3.tf
@@ -14,16 +14,17 @@ resource "aws_s3_bucket_policy" "mlflow_bucket_policy" {
     Version = "2012-10-17",
     Statement = [
       {
-        Effect = "Allow",
+        Sid       = "AllowMlflowEC2RoleAccess",
+        Effect    = "Allow",
         Principal = {
           AWS = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/mlflow-ec2-role-eu-west-1"
         },
-        Action = [
+        Action    = [
           "s3:ListBucket",
           "s3:GetObject",
           "s3:PutObject"
         ],
-        Resource = [
+        Resource  = [
           "${aws_s3_bucket.mlflow_artifacts.arn}",
           "${aws_s3_bucket.mlflow_artifacts.arn}/*"
         ]
