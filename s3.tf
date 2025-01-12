153b00fb566fade6a3325719a2265ce2ae66f6dd
metodija747 <metodijabucevski@yahoo.com>
2025-01-12 09:21:02 +0100

    fsfsdfs

--- s3.tf
+++ s3.tf
@@ -7,3 +7,27 @@ resource "aws_s3_bucket" "mlflow_artifacts" {
     prevent_destroy = true
   }
 }
+
+resource "aws_s3_bucket_policy" "mlflow_bucket_policy" {
+  bucket = aws_s3_bucket.mlflow_artifacts.id
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = {
+          AWS = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/mlflow-ec2-role-eu-west-1"
+        },
+        Action = [
+          "s3:ListBucket",
+          "s3:GetObject",
+          "s3:PutObject"
+        ],
+        Resource = [
+          "${aws_s3_bucket.mlflow_artifacts.arn}",
+          "${aws_s3_bucket.mlflow_artifacts.arn}/*"
+        ]
+      }
+    ]
+  })
+}
